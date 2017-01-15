f414df4322087c430e1e1f0b90066d1e12b3b0f8
Naki21 <Yianni21@gmail.com>
2017-01-15 13:14:48 -0500

    Add cancel contest functionality
    Added a cancel button which will properly cancel the creation of a new
    contest on click and will roll back any inputs which may have been
     entered in the input fields.

--- app/contests/new/route.js
+++ app/contests/new/route.js
@@ -3,13 +3,18 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model() {
-  return this.get('store').createRecord('contest', {});
-},
-actions: {
-  newContest(contest) {
-    contest.save();
-    console.log(contest);
-    this.transitionTo('contests');
+    return this.get('store').createRecord('contest');
+  },
+  actions: {
+    newContest(contest) {
+      contest.save();
+      console.log(contest);
+      this.transitionTo('contests');
+    },
+    cancelNewContest(contest) {
+      contest.rollbackAttributes();
+      this.transitionTo('contests');
+      console.log('cancel route');
+    }
   }
-}
 });
