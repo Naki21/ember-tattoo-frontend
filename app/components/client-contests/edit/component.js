0818e3b7e875daf8807521756465fd1f64fde9f9
Naki21 <Yianni21@gmail.com>
2017-01-16 18:18:06 -0500

    Add edit function to contest
    Adds the finction to update contests to persist through to the backend.
    Added the actions to the button, up the component chain and up to the
    route.

--- app/components/client-contests/edit/component.js
+++ app/components/client-contests/edit/component.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    save() {
+      console.log('in component edit save');
+      console.log(this.get('contest'));
+      this.sendAction('save', this.get('contest'));
+    },
+    cancel() {
+      this.sendAction('cancel', this.get('contest'));
+    }
+  }
 });
--- app/contest/edit/route.js
+++ app/contest/edit/route.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
+  actions: {
+    editContest(contest){
+      console.log(contest);
+      console.log(this.get('contest.prize'));
+      console.log('here');
+      contest.save();
+      this.transitionTo('contest', contest.id);
+    }
+  }
 });
