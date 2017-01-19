1336bab88e0eb0deaa3efaa205bab307444fad84
Naki21 <Yianni21@gmail.com>
2017-01-18 20:32:11 -0500

    Remove useless user button
    Removed button that lists all users.

--- app/router.js
+++ app/router.js
@@ -9,7 +9,6 @@ Router.map(function () {
   this.route('sign-up');
   this.route('sign-in');
   this.route('change-password');
-  this.route('users');
   this.route('contests', function() {
     this.route('new');
   });
