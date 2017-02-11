b24931a99657be2f0e366d2821eda0706a35d8f6
Naki21 <Yianni21@gmail.com>
2017-02-11 16:05:32 -0500

    Add new header for the contest info display
    Added the bootstrap HTML and CSS for rendering a new, cleaner looking
    information header for the contest info/submission listing page.
    Next steps: breakdown the code snippit into smaller component pieces
    and integrate into contests/contest route template

--- app/components/client-contests/info/component.js
+++ app/components/client-contests/info/component.js
@@ -1,8 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'div',
-  classNames: ['page-header'],
   actions: {
     edit(){
     console.log('in the info comp', this.get('contest'));
